6f798fd5ff7b4297a36244b71e2aedbd6be3d996
Savio-Miranda <saviomiranda.dev@gmail.com>
2025-01-23 16:05:13 -0300

    mudança na entrada da bissecção

--- bisseccao.py
+++ bisseccao.py
@@ -1,20 +1,25 @@
-import numpy as np
-
-
-def bissection(coeficients: list, a: int, b: int, max_iter: int):
-    function = np.poly1d(coeficients)
+def bissection(a: float, b: float, max_iter: int, error: float, f = lambda x: ...):
     for i in range(max_iter):
         midpoint = (a + b)/2
-        chech_signal = float(function(a)) * float(function(midpoint))
-        if chech_signal < 0:
+        check_signal = f(a) * f(midpoint)
+        if check_signal < 0:
             b = midpoint
-        elif chech_signal > 0:
+        elif check_signal > 0:
             a = midpoint
         else:
+            return midpoint
+        
+        if abs(f(midpoint)) < error:
             return midpoint
 
     return midpoint
 
 
-root = bissection([3, -4, -2, 1], 1, 2, 1000)
+root = bissection(-4, -2, 1000, 0.0000001, lambda x: (x**4)-(9*x**2)+(3*x)+12)
+print(f"A raiz encontrada é: {root}")
+root = bissection(-2, 1, 1000, 0.0000001, lambda x: (x**4)-(9*x**2)+(3*x)+12)
+print(f"A raiz encontrada é: {root}")
+root = bissection(1, 2, 1000, 0.0000001, lambda x: (x**4)-(9*x**2)+(3*x)+12)
+print(f"A raiz encontrada é: {root}")
+root = bissection(2, 3, 1000, 0.0000001, lambda x: (x**4)-(9*x**2)+(3*x)+12)
 print(f"A raiz encontrada é: {root}")
