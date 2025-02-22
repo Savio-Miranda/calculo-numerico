423ac1efbc755590b2ecf54305712c3529a7c766
Savio-Miranda <saviomiranda.dev@gmail.com>
2025-02-22 19:06:14 -0300

    adição de notas

--- testes_e_exemplos/ajuste_minimos_quadrados_example.py
+++ testes_e_exemplos/ajuste_minimos_quadrados_example.py
@@ -7,22 +7,29 @@ from plot.plot import plot
 
 
 ### EXEMPLO DE USO ###
-def funcao_modelo(x, alpha1, alpha2):
-    return alpha2*np.exp(alpha1*x)
 
+## Exemplo exponencial ##
 def f(x):
-    return funcao_modelo(x, coefs[0], coefs[1])
+    return coefs[1]*np.exp(coefs[0]*x) # função real, sem tirar nem pôr
 
-
-# Dados de entrada
-X = np.array([-1, -0.7, -0.4, -0.1, 0.2, 0.5, 0.8, 1])
-Y = np.array([36.547, 17.264, 8.155, 3.852, 1.820, 0.860, 0.406, 0.246])
+X = [-1, -0.7, -0.4, -0.1, 0.2, 0.5, 0.8, 1]
+Y = [36.547, 17.264, 8.155, 3.852, 1.820, 0.860, 0.406, 0.246]
 Y_ajustado = Y.copy()
-Y_ajustado = ajustar_valores(Y_ajustado, lambda y: np.log(y))
+Y_ajustado = ajustar_valores(Y_ajustado, lambda y: np.log(y)) # ajuste de valores em Y
 
-
-g_list = [lambda x: np.exp(x), lambda x: 1]
+g_list = [lambda a: np.exp(a), lambda b: 1] # se a função é b*e^(a*x), então são necessários dois coeficientes
 
 coefs = minimos_quadrados_nao_lineares(X, Y_ajustado, g_list, resolver_sistema_lu)
+print(coefs)
+plot(X, Y, f, -2,2, "MMQ")
 
-plot(X, Y, f, -2, 2, "MMQ")
+## Exemplo Linear ##
+# def f(x):
+#     return coefs[0]*x + coefs[1]
+#
+#
+# X = [1, 3, 4]
+# Y = [3, 7, 9]
+# g_list = [lambda x: x, lambda x: 1]
+# coefs = minimos_quadrados_nao_lineares(X, Y, g_list, resolver_sistema_lu)
+
