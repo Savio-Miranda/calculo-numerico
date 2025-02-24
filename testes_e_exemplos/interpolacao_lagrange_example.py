c3c388e10b2da42e207e2240df7792d3739196cc
Savio-Miranda <saviomiranda.dev@gmail.com>
2025-02-24 10:44:00 -0300

    comentários necessários em MMQ

--- testes_e_exemplos/interpolacao_lagrange_example.py
+++ testes_e_exemplos/interpolacao_lagrange_example.py
@@ -7,7 +7,9 @@ def f(x):
     return lagrange(X, Y, x)
 
 
-X = [1, 2, 3]
-Y = [0, 1, 4]
+# X = [1, 2, 3]
+# Y = [0, 1, 4]
+X = [-2, 0, 1, 2]
+Y = [-47, -3, 0, 41]
 
-plot(X, Y, f, 0, 4, "Lagrange")
+plot(X, Y, f, -3, 3, "Interpolação de Lagrange")
